12d77f28d5d9fa4c1d2951012b6cab4fd1c83423
m.v.u <mrt.v.urisman@gmail.com>
2021-09-01 11:44:24 -0400

    testeando

--- backend/Cliente/models.py
+++ backend/Cliente/models.py
@@ -2,10 +2,12 @@ from django.db import models
 
 # Create your models here.
 
-#Clase Registro
+#Tabla registro cliente
 class Register(models.Model):
     name=models.CharField(max_length=30)
     last_name=models.CharField(max_length=30)
     email=models.EmailField()
     password=models.CharField(max_length=30)
     confirm_password=models.CharField(max_length=30)
+
+# AGREGANDO NUEVOS CAMBIOS jeje
